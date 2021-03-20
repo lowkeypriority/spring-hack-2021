5c6491fe8064e229906609792af66e65cc28ab54
Daniel <74528634+Daniel-Borovoy@users.noreply.github.com>
2021-03-20 16:32:20 +0300

    Multiplayer
    Осталась проблема с следование камеры

---
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/NetPalyer.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetPalyer : NetworkBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rigidBodyComponent;
+    public Camera cameraObject;
+
+    Vector2 movement;
+    Vector2 mousePosition;
+
+    private void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePosition = cameraObject.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        rigidBodyComponent.MovePosition(rigidBodyComponent.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDirection = mousePosition - rigidBodyComponent.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+
+        rigidBodyComponent.rotation = angle;
+    }
+}
