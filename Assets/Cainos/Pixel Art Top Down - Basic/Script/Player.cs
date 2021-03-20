2a40a3156e334c9ef69dc535f16fd168cdbf4aa1
Oleg <akimin97@mail.ru>
2021-03-20 17:22:40 +0300

    +Jerk

--- Assets/Cainos/Pixel Art Top Down - Basic/Script/Player.cs
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/Player.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Collections;
 
 public class Player : MonoBehaviour
 {
@@ -9,8 +8,13 @@ public class Player : MonoBehaviour
     public Rigidbody2D rigidBodyComponent;
     public Camera cameraObject;
 
+    private float jerkTime = 10;
+    private bool isJerk = false;
+
     Vector2 movement;
     Vector2 mousePosition;
+
+
 
     private void Update()
     {
@@ -18,15 +22,31 @@ public class Player : MonoBehaviour
         movement.y = Input.GetAxisRaw("Vertical");
 
         mousePosition = cameraObject.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetKey(KeyCode.R) && !isJerk)
+        {
+            StartCoroutine(Jerk());
+        }
+    }
+
+
+    private IEnumerator Jerk()
+    {
+        isJerk = true;
+        moveSpeed *= 6f;
+        yield return new WaitForSeconds(0.1f);
+        moveSpeed = 5f;
+        yield return new WaitForSeconds(jerkTime);
+        isJerk = false;
     }
 
     private void FixedUpdate()
-    {
-        rigidBodyComponent.MovePosition(rigidBodyComponent.position + movement * moveSpeed * Time.fixedDeltaTime);
+        {
+            rigidBodyComponent.MovePosition(rigidBodyComponent.position + movement * moveSpeed * Time.fixedDeltaTime);
 
-        Vector2 lookDirection = mousePosition - rigidBodyComponent.position;
-        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+            Vector2 lookDirection = mousePosition - rigidBodyComponent.position;
+            float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
 
-        rigidBodyComponent.rotation = angle;
+            rigidBodyComponent.rotation = angle;
     }
 }
